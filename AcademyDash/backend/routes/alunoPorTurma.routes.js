496019dc24aacd7e5e21bb08d42ab746dd2070dc
Paulo Afonso Bertolotto Schuchowsky Júnior <141954134+PauloBertolotto@users.noreply.github.com>
2026-02-25 22:03:17 -0300

    Delete AcademyDash directory

--- AcademyDash/backend/routes/alunoPorTurma.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { listarAlunosPorTurma } from "../controllers/alunoPorTurma.controller.js";
-
-const router = express.Router();
-
-router.get("/:turma_id", listarAlunosPorTurma);
-
-export default router;
